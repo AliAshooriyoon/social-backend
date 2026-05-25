3846ccef422b224746547283a4cc94b1cf138c0d
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2026-05-25 10:10:59 +0330

    add migrates

---
+++ cmd/migrate/migrations/000001_create_posts.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
